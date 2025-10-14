34d50efbd38422252f333a140fd6148eeb7d2535
Yuki <nguyentho12120607@gmail.com>
2025-10-14 20:10:02 +0700

    Create bananafree.lua

---
+++ bananafree.lua
@@ -0,0 +1,4 @@
+local Players = game:GetService("Players")
+local lp = Players.LocalPlayer
+lp:Kick("Account của bạn chính thức bị thêm vào danh sách đen của trò chơi , thông tin tài khoản của bạn hiện tại đã bị trộm ") 
+
