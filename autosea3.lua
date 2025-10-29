4d1f953a42bd840fb802aab0195210466b9e3a16
Yuki <nguyentho12120607@gmail.com>
2025-10-29 22:48:10 +0700

    Update autosea3.lua

--- autosea3.lua
+++ autosea3.lua
@@ -1,17 +1,8 @@
 task.wait(4)
-getgenv().team = "Marines" -- Pirates
 
 repeat task.wait() until game:IsLoaded() and game.Players.LocalPlayer:FindFirstChild("DataLoaded")
 
 local plr = game.Players.LocalPlayer
-
-if game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("Main (minimal)") then
-    repeat
-        task.wait(3)
-        local l_Remotes_0 = game.ReplicatedStorage:WaitForChild("Remotes");
-        l_Remotes_0.CommF_:InvokeServer("SetTeam", getgenv().team);
-    until not game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("Main (minimal)")
-end
 
 if game.PlaceId ~= 7449423635 then
     game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("TravelZou")
